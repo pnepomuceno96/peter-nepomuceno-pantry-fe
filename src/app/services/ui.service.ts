fd9d59e3c045123725df4c2ce5964a3e20dce90e
pnepomuceno <pn7000@gmail.com>
2023-01-26 14:26:37 -0600

    filters items

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Observable, Subject, take } from 'rxjs';
 import { AppUser } from 'src/data/AppUser';
 import { CookedRecipe } from 'src/data/CookedRecipe';
@@ -45,6 +46,11 @@ export class UiService {
 
     this.$items.subscribe({next: items => {
       this.sortedItems = items.slice()
+      this.dataSource = new MatTableDataSource(this.sortedItems)
+      this.dataSource.filterPredicate = (data: any, filter) => {
+        const dataStr = JSON.stringify(data).toLowerCase();
+        return dataStr.indexOf(filter) != -1;
+      }
     }})
     
     this.$cookedRecipes.subscribe({next: cookedRecipes => {
@@ -52,6 +58,7 @@ export class UiService {
     }})
     console.log(this.currentUser)
     
+    //this.dataSource = new MatTableDataSource(this.sortedItems)
   }
   private userUrl = "http://localhost:8080/appusers"
   private itemUrl = "http://localhost:8080/items"
@@ -272,15 +279,17 @@ export class UiService {
     return this.cookedRecipe
   }
 
+  
+
   sortedItems: Item[] = []
   sortItems(sort: Sort) {
     const data = this.items.slice();
     if (!sort.active || sort.direction === '') {
-      this.sortedItems = data;
+      this.dataSource.filteredData = data;
       return;
     }
 
-    this.sortedItems = data.sort((a, b) => {
+    this.dataSource.filteredData = data.sort((a, b) => {
       const isAsc = sort.direction === 'asc';
       switch (sort.active) {
         case 'name':
@@ -341,6 +350,13 @@ export class UiService {
           return 0;
       }
     });
+  }
+
+  dataSource= new MatTableDataSource(this.sortedItems)
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log(this.sortedItems)
   }
 
   // public watchUser(): Observable<AppUser> {
