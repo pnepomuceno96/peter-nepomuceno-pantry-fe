97066625245d22df03d3cd5583fe83733b8b0959
pnepomuceno <pn7000@gmail.com>
2023-01-26 10:34:20 -0600

    card image height formatting

--- src/data/Ingredient.ts
+++ src/data/Ingredient.ts
@@ -2,8 +2,7 @@ import { Item } from "./Item"
 
 export interface Ingredient {
     id: number
-    //TODO: Change reference variable to item name so that recipes can reference it again after deletion and recreation
-    //itemNo: number
+    
     name: string
     measurement: string
     quantity: number
